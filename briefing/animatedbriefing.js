dbe86395b8cd23a16022e51e0b96dad9c1f88850
realmarble <glabusjakub@gmail.com>
2024-02-23 11:07:14 +0100

    formatting and cleanup

--- briefing/animatedbriefing.js
+++ briefing/animatedbriefing.js
@@ -1,36 +1,20 @@
 async function Briefing(object) {
-  typeitid = randomID(36)
-  object.RandomID = typeitid
+  typeitid = randomID(36);
+  object.RandomID = typeitid;
   introhtml = "";
-  if ((object.Intro = true)) {
-    try {
-      introhtml = await renderTemplate(`modules/moss-lancer/templates/briefings/intros/${object.IntroData.type}.hbs`,object)
-    } catch (error) {
-      introhtml = `
-    <div id="standby" style="width:100%;height:100%;background-size: 40px 40px;
-  background-image: radial-gradient(circle, ${object.BackgroundAccent} 1px, rgba(0, 0, 0, 0) 1px);background-color: ${object.BackgroundColor};font-family: 'Ubuntu Mono', monospace;
-  color: ${object.TextColor};">
-  <script>
-    document.getElementById("standby").style.display="none"
-  document.getElementById("briefingcontainer").style.display="block"
-    </script>
-    </div>
-    `;
-    }
+  if (object.Intro == true) {
+    introhtml = await renderTemplate(`modules/moss-lancer/templates/briefings/intros/${object.IntroData.type}.hbs`,object);
+  } else {
+    introhtml = await renderTemplate(`modules/moss-lancer/templates/briefings/intros/none.hbs`,object);
   }
+
   try {
-    introhtml = await renderTemplate(`modules/moss-lancer/templates/briefings/intros/${object.IntroData.type}.hbs`,object)  
+    briefinghtml = await renderTemplate(`modules/moss-lancer/templates/briefings/layouts/${object.LayoutType}.hbs`,object);
   } catch (error) {
-    ui.notifications.error("Error in Intro Params:",e);
-    return
+    ui.notifications.error(`Error in Briefing Params: ${error}`);
+    return;
   }
-  try {
-    briefinghtml = await renderTemplate(`modules/moss-lancer/templates/briefings/layouts/${object.LayoutType}.hbs`,object)
-  } catch (error) {
-    ui.notifications.error("Error in Briefing Params:",e);
-    return
-  }
-  let brief = new BriefingWindow(String.raw`${introhtml + briefinghtml}`,typeitid)
+  let brief = new BriefingWindow(String.raw`${introhtml + briefinghtml}`,typeitid);
   brief.render(true);
 }
 
@@ -42,9 +26,9 @@ class BriefingEditor extends BaseEditor {
 }
 
 class BriefingWindow extends Application {
-  constructor(content,typeitid = randomID(10)) {
+  constructor(content, typeitid = randomID(10)) {
     super();
-    this.content = content
+    this.content = content;
     this.typeitid = typeitid;
   }
   static get defaultOptions() {
@@ -61,8 +45,7 @@ class BriefingWindow extends Application {
   getData() {
     const data = super.getData();
     data.html = this.content;
-    data.RandomID = this.typeitid //this is for supporting multiple instances of windows with dynamically typed text.
+    data.RandomID = this.typeitid; //this is for supporting multiple instances of windows with dynamically typed text.
     return data;
   }
 }
-
