5da9639545065496bc58338a9409cfbb60baf0e1
realmarble <glabusjakub@gmail.com>
2026-02-03 10:59:57 +0100

    stability fixes

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 Hooks.on("ready", function() {
 //console.log("moss-lancer | registering stuff...")
+    if (MOSS.LocalStorage.Read() == -1) {
+        MOSS.LocalStorage.Set([]);
+        console.log("moss-lancer | LocalStorage failed! Regenerating...")
+    }
+    //failsafe for incorrectly set up LocalStorage
 });
